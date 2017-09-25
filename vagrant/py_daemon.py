3a83214a7fcc17982c9d70791e1713d69c1813f2
John Puskar <jpuskar@us.ibm.com>
2017-09-24 21:02:10 -0400

    Thread kills properly.

---
+++ vagrant/py_daemon.py
@@ -0,0 +1,42 @@
+import daemon
+import lockfile
+import signal
+import time
+
+
+class TestDaemon(object):
+
+    def __init__(self):
+        self.stopped = False
+
+    def stop_tasks(self):
+        self.stopped = True
+
+    def run(self):
+        i = 0
+        # self.stopped = False
+        logfile = open('/root/py_daemon.log', 'a')
+        logfile.writelines("Starting run.\n")
+        while self.stopped is False:  # or i < 3:
+            # logfile.writelines("run " + str(i) + "\n")
+            logfile.writelines("run " + str(i) + "\n")
+            i = i + 1
+            # time.sleep(1)
+        logfile.writelines("exited cleanly at " + str(i) + "\n")
+        logfile.close()
+
+
+td = TestDaemon()
+
+
+def program_cleanup(signum, frame):
+    td.stop_tasks()
+
+
+context = daemon.DaemonContext()
+context.signal_map = {
+    signal.SIGTERM: program_cleanup,
+}
+
+with context:
+    td.run()
